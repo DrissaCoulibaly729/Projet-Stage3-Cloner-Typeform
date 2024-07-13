774df3b45ee6eb522922442f0868e94636035b13
Nymosdt <dc377303@gmail.com>
2024-07-13 00:02:37 +0000

    new commit

--- src/pages/FormContent.js
+++ src/pages/FormContent.js
@@ -1,127 +1,192 @@
 import React from 'react';
-import {
-  AiOutlineFontSize,
-  AiOutlineCheckSquare,
-  AiOutlineFileText,
-  AiOutlineMail,
-  AiOutlinePhone,
-  AiOutlineGlobal,
-  AiOutlineStar,
-  AiOutlineCalendar,
-  AiOutlineFileAdd,
-  AiOutlineGoogle,
-  AiOutlineEye,
-} from 'react-icons/ai';
-import { FaRegAddressCard, FaRegImages } from 'react-icons/fa';
-import { MdOutlineCategory, MdOutlineFormatListBulleted } from 'react-icons/md';
-import { BiToggleLeft, BiText, BiUpload } from 'react-icons/bi';
-import { RiPagesLine } from 'react-icons/ri';
-import {  FiChevronDown } from 'react-icons/fi';
 
 const categories = [
   {
     name: 'Recommended',
     items: [
-      { label: 'Short Text', icon: AiOutlineFontSize, bgColor: 'bg-blue-100' },
-      { label: 'Multiple Choice', icon: AiOutlineCheckSquare, bgColor: 'bg-purple-100' },
-      { label: 'Statement', icon: AiOutlineFileText, bgColor: 'bg-gray-100' },
-      { label: 'Opinion Scale', icon: AiOutlineStar, bgColor: 'bg-green-100' },
-      { label: 'Email', icon: AiOutlineMail, bgColor: 'bg-yellow-100' },
+      { label: 'Short Text', bgColor: 'bg-blue-100' },
+      { label: 'Multiple Choice', bgColor: 'bg-purple-100' },
+      { label: 'Statement', bgColor: 'bg-gray-100' },
+      { label: 'Opinion Scale', bgColor: 'bg-green-100' },
+      { label: 'Email', bgColor: 'bg-yellow-100' },
     ],
   },
   {
     name: 'Contact Info',
     items: [
-      { label: 'Contact Info', icon: FaRegAddressCard, bgColor: 'bg-yellow-100' },
-      { label: 'Email', icon: AiOutlineMail, bgColor: 'bg-yellow-100' },
-      { label: 'Phone Number', icon: AiOutlinePhone, bgColor: 'bg-yellow-100' },
-      { label: 'Address', icon: AiOutlineGlobal, bgColor: 'bg-yellow-100' },
-      { label: 'Website', icon: AiOutlineGlobal, bgColor: 'bg-yellow-100' },
+      { label: 'Contact Info', bgColor: 'bg-yellow-100' },
+      { label: 'Email', bgColor: 'bg-yellow-100' },
+      { label: 'Phone Number', bgColor: 'bg-yellow-100' },
+      { label: 'Address', bgColor: 'bg-yellow-100' },
+      { label: 'Website', bgColor: 'bg-yellow-100' },
     ],
   },
   {
     name: 'Choice',
     items: [
-      { label: 'Multiple Choice', icon: AiOutlineCheckSquare, bgColor: 'bg-purple-100' },
-      { label: 'Dropdown', icon: FiChevronDown, bgColor: 'bg-purple-100' },
-      { label: 'Picture Choice', icon: FaRegImages, bgColor: 'bg-purple-100' },
-      { label: 'Yes/No', icon: BiToggleLeft, bgColor: 'bg-purple-100' },
-      { label: 'Legal', icon: MdOutlineCategory, bgColor: 'bg-purple-100' },
+      { label: 'Multiple Choice', bgColor: 'bg-purple-100' },
+      { label: 'Dropdown', bgColor: 'bg-purple-100' },
+      { label: 'Picture Choice', bgColor: 'bg-purple-100' },
+      { label: 'yes_no', bgColor: 'bg-purple-100' },
+      { label: 'Legal', bgColor: 'bg-purple-100' },
     ],
   },
   {
     name: 'Rating & Ranking',
     items: [
-      { label: 'Net Promoter Score®', icon: AiOutlineStar, bgColor: 'bg-green-100' },
-      { label: 'Opinion Scale', icon: AiOutlineStar, bgColor: 'bg-green-100' },
-      { label: 'Rating', icon: AiOutlineStar, bgColor: 'bg-green-100' },
-      { label: 'Ranking', icon: MdOutlineFormatListBulleted, bgColor: 'bg-green-100' },
-      { label: 'Matrix', icon: MdOutlineCategory, bgColor: 'bg-green-100' },
+      { label: 'Net Promoter Score', bgColor: 'bg-green-100' },
+      { label: 'Opinion Scale', bgColor: 'bg-green-100' },
+      { label: 'Rating', bgColor: 'bg-green-100' },
+      { label: 'Ranking', bgColor: 'bg-green-100' },
+      { label: 'Matrix', bgColor: 'bg-green-100' },
     ],
   },
   {
     name: 'Text',
     items: [
-      { label: 'Long Text', icon: BiText, bgColor: 'bg-blue-100' },
-      { label: 'Short Text', icon: BiText, bgColor: 'bg-blue-100' },
-      { label: 'Video', icon: AiOutlineEye, bgColor: 'bg-orange-100' },
+      { label: 'Long Text', bgColor: 'bg-blue-100' },
+      { label: 'Short Text', bgColor: 'bg-blue-100' },
+      { label: 'Video', bgColor: 'bg-orange-100' },
     ],
   },
   {
     name: 'Other',
     items: [
-      { label: 'Number', icon: AiOutlineFileAdd, bgColor: 'bg-yellow-100' },
-      { label: 'Date', icon: AiOutlineCalendar, bgColor: 'bg-yellow-100' },
-      { label: 'Payment', icon: AiOutlineFileAdd, bgColor: 'bg-yellow-100' },
-      { label: 'File Upload', icon: BiUpload, bgColor: 'bg-yellow-100' },
-      { label: 'Google Drive', icon: AiOutlineGoogle, bgColor: 'bg-yellow-100' },
-      { label: 'Calendly', icon: AiOutlineCalendar, bgColor: 'bg-yellow-100' },
+      { label: 'Number', bgColor: 'bg-yellow-100' },
+      { label: 'Date', bgColor: 'bg-yellow-100' },
+      { label: 'Payment', bgColor: 'bg-yellow-100' },
+      { label: 'File Upload', bgColor: 'bg-yellow-100' },
+      { label: 'Google Drive', bgColor: 'bg-yellow-100' },
+      { label: 'Calendly', bgColor: 'bg-yellow-100' },
     ],
   },
   {
     name: 'Welcome Screen',
     items: [
-      { label: 'Welcome Screen', icon: RiPagesLine, bgColor: 'bg-gray-100' },
-      { label: 'Statement', icon: AiOutlineFileText, bgColor: 'bg-gray-100' },
-      { label: 'Question Group', icon: AiOutlineFileAdd, bgColor: 'bg-gray-100' },
-      { label: 'Multi-Question Page', icon: RiPagesLine, bgColor: 'bg-gray-100' },
-      { label: 'End Screen', icon: RiPagesLine, bgColor: 'bg-gray-100' },
-      { label: 'Redirect to URL', icon: AiOutlineGlobal, bgColor: 'bg-gray-100' },
+      { label: 'Welcome Screen', bgColor: 'bg-gray-100' },
+      { label: 'Statement', bgColor: 'bg-gray-100' },
+      { label: 'Question Group', bgColor: 'bg-gray-100' },
+      { label: 'Multi Question Page', bgColor: 'bg-gray-100' },
+      { label: 'End Screen', bgColor: 'bg-gray-100' },
+      { label: 'Redirect to URL', bgColor: 'bg-gray-100' },
     ],
   },
 ];
 
 const FormContent = () => {
+  const getImageSrc = (label) => {
+    return `/svg/${label.toLowerCase().replace(/ /g, '_')}.svg`;
+  };
+
   return (
     <div className="flex justify-center">
-      <div className="bg-white rounded-lg shadow-lg w-full max-w-6xl">
-        <div className="border-b p-4 flex justify-between items-center">
-          <div className="text-lg font-semibold">My workspace &gt; My new form</div>
-          <div className="flex space-x-4">
-            <button className="bg-gray-200 px-4 py-2 rounded">Add content</button>
-            <button className="bg-gray-200 px-4 py-2 rounded">Import questions</button>
-            <button className="bg-gray-200 px-4 py-2 rounded">Create with AI</button>
-            <button className="bg-gray-200 px-4 py-2 rounded">Qualify leads</button>
-          </div>
+      <div className="bg-stone-100 rounded-lg shadow-lg w-full max-w-6xl">
+        <div className="p-4 flex justify-between items-center">
+          <nav>
+            <ul className="flex space-x-4">
+              <li className="text-sm"><a href="/">Add content</a></li>
+              <li className="text-sm"><a href="/">Import questions</a></li>
+              <li className="text-sm"><a href="/">Create with AI</a></li>
+              <li className="text-sm"><a href="/">Qualify leads</a></li>
+            </ul>
+          </nav>
         </div>
-        <div className="p-4">
-          <div className="grid grid-cols-2 gap-4">
-            {categories.map((category) => (
-              <div key={category.name}>
-                <h3 className="mb-2 text-sm font-medium text-gray-600">{category.name}</h3>
-                <div className="grid grid-cols-2 gap-2">
-                  {category.items.map((item, index) => (
-                    <div
-                      key={index}
-                      className={`flex items-center p-2 ${item.bgColor} rounded`}
-                    >
-                      <item.icon className="mr-2 text-gray-600" />
-                      <span className="text-sm">{item.label}</span>
+        <div className="p-4 bg-white m-2 mb-0 mr-0 rounded-l-3xl rounded-br-3xl">
+          <div className="grid grid-cols-4 gap-4">
+            <div>
+              <div className="flex border p-1 rounded-lg w-full items-center mb-4">
+                <img src="/svg/search.svg" alt="Search" className="h-6 w-6 mr-2" />
+                <input type="text" placeholder="Search" className="border-none focus:outline-none w-full" />
+              </div>
+              <div>
+                <h3 className="mb-2 text-sm font-medium text-gray-600">Recommended</h3>
+                <div className="grid grid-cols-1 gap-2">
+                  {categories[0].items.map((item, index) => (
+                    <div key={index} className="flex items-center p-1 rounded-lg border">
+                      <div className={`flex items-center justify-center p-2 rounded-lg ${item.bgColor}`}>
+                        <img src={getImageSrc(item.label)} className="h-2 w-2 md:h-4 md:w-4" alt={item.label} />
+                      </div>
+                      <span className="ml-2 text-xs md:text-sm">{item.label}</span>
                     </div>
                   ))}
                 </div>
               </div>
-            ))}
+            </div>
+            <div>
+              <h3 className="mb-2 text-sm font-medium text-gray-600">Contact Info</h3>
+              <div className="grid grid-cols-1 gap-2 mb-4">
+                {categories[1].items.map((item, index) => (
+                  <div key={index} className="flex items-center p-1 rounded-lg">
+                    <div className={`flex items-center justify-center p-2 rounded-lg ${item.bgColor}`}>
+                      <img src={getImageSrc(item.label)} className="h-2 w-2 md:h-4 md:w-4" alt={item.label} />
+                    </div>
+                    <span className="ml-2 text-xs md:text-sm">{item.label}</span>
+                  </div>
+                ))}
+              </div>
+              <h3 className="mb-2 text-sm font-medium text-gray-600">Text</h3>
+              <div className="grid grid-cols-1 gap-2">
+                {categories[4].items.map((item, index) => (
+                  <div key={index} className="flex items-center p-1 rounded-lg">
+                    <div className={`flex items-center justify-center p-2 rounded-lg ${item.bgColor}`}>
+                      <img src={getImageSrc(item.label)} className="h-2 w-2 md:h-4 md:w-4" alt={item.label} />
+                    </div>
+                    <span className="ml-2 text-xs md:text-sm">{item.label}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div>
+              <h3 className="mb-2 text-sm font-medium text-gray-600">Choice</h3>
+              <div className="grid grid-cols-1 gap-2 mb-4">
+                {categories[2].items.map((item, index) => (
+                  <div key={index} className="flex items-center p-1 rounded-lg">
+                    <div className={`flex items-center justify-center p-2 rounded-lg ${item.bgColor}`}>
+                      <img src={getImageSrc(item.label)} className="h-2 w-2 md:h-4 md:w-4" alt={item.label} />
+                    </div>
+                    <span className="ml-2 text-xs md:text-sm">{item.label}</span>
+                  </div>
+                ))}
+              </div>
+              <h3 className="mb-2 text-sm font-medium text-gray-600">Other</h3>
+              <div className="grid grid-cols-1 gap-2">
+                {categories[5].items.map((item, index) => (
+                  <div key={index} className="flex items-center p-1 rounded-lg">
+                    <div className={`flex items-center justify-center p-2 rounded-lg ${item.bgColor}`}>
+                      <img src={getImageSrc(item.label)} className="h-2 w-2 md:h-4 md:w-4" alt={item.label} />
+                    </div>
+                    <span className="ml-2 text-xs md:text-sm">{item.label}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div>
+              <h3 className="mb-2 text-sm font-medium text-gray-600">Rating & Ranking</h3>
+              <div className="grid grid-cols-1 gap-2 mb-4">
+                {categories[3].items.map((item, index) => (
+                  <div key={index} className="flex items-center p-2 rounded-lg">
+                    <div className={`flex items-center justify-center p-2 rounded-lg ${item.bgColor}`}>
+                      <img src={getImageSrc(item.label)} className="h-2 w-2 md:h-4 md:w-4" alt={item.label} />
+                    </div>
+                    <span className="ml-2 text-xs md:text-sm">{item.label}</span>
+                  </div>
+                ))}
+              </div>
+              <div className="grid grid-cols-1 gap-2">
+                {categories.slice(6).map((category, catIndex) => (
+                  <div key={catIndex}>
+                    {category.items.map((item, index) => (
+                      <div key={index} className="flex items-center p-2 rounded-lg">
+                        <div className={`flex items-center justify-center p-2 rounded-lg ${item.bgColor}`}>
+                          <img src={getImageSrc(item.label)} className="h-2 w-2 md:h-4 md:w-4" alt={item.label} />
+                        </div>
+                        <span className="ml-2 text-xs md:text-sm">{item.label}</span>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
